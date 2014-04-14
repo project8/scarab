d7e0fa877c8de67135728328b1f98b8e6b60d10f
Ben LaRoque <benjamin.laroque@gmail.com>
2014-04-13 17:29:56 -0700

    updates\!

--- post-merge.sh
+++ post-merge.sh
@@ -3,7 +3,3 @@
 echo "Doing post merge"
 
 bash pre-commit.sh
-
-echo `pwd`
-
-echo $@
--- pre-commit.sh
+++ pre-commit.sh
@@ -5,6 +5,7 @@ echo "doing pre-commit hook"
 if [ $(git symbolic-ref --short HEAD) == "master" ]
 then
   echo "on master, bumping"
+  python bump_version.py
 else
   echo "not on master, not bumping"
 fi
