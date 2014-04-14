284bdc3def3d641cdd37031f1f94cc116879e8af
Ben LaRoque <benjamin.laroque@gmail.com>
2014-04-13 18:34:38 -0700

    fixing linking

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-ln -s post-merge.sh .git/hooks/post-merge
-ln -s pre-commit.sh .git/hooks/pre-commit
+ln -s ../../post-merge.sh .git/hooks/post-merge
+ln -s ../../pre-commit.sh .git/hooks/pre-commit
