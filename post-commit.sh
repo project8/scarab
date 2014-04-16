a0b6491cadd81cd109e5196aab79d4d69ba29cf4
Ben LaRoque <benjamin.laroque@gmail.com>
2014-04-16 16:58:14 -0700

    removing excess screen printing

--- post-commit.sh
+++ post-commit.sh
@@ -4,10 +4,9 @@ echo "doing post-commit hook"
 
 if [ $(git symbolic-ref --short HEAD) == "master" ]
 then
-  echo "on master, tagging"
   tag_name=`python -c "print('v'+'.'.join(open('VERSION').readlines()[0].split()))"`
   git tag -a $tag_name -m "automatically tagging via post-commit hook"
-  echo "tag is $tag_name"
+  echo "automatically created tag: $tag_name"
 else
   echo "not on master, not bumping"
 fi
