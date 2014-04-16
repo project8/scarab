eec5e84575d08d748253c4ba1c368d9bc3b3c4db
Ben LaRoque <benjamin.laroque@gmail.com>
2014-04-16 16:20:39 -0700

    like this?

--- post-commit.sh
+++ post-commit.sh
@@ -5,7 +5,7 @@ echo "doing post-commit hook"
 if [ $(git symbolic-ref --short HEAD) == "master" ]
 then
   echo "on master, tagging"
-  tag_name = python -c "print('v'+'.'.join(open('VERSION').readlines()[0].split()))"
+  tag_name=`python -c "print('v'+'.'.join(open('VERSION').readlines()[0].split()))"`
   echo "tag is $tag_name"
 else
   echo "not on master, not bumping"
