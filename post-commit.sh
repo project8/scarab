74e973ac588d972cb6ef9560889c854b314a85b6
Ben LaRoque <benjamin.laroque@gmail.com>
2014-04-16 16:22:52 -0700

    adding tagging

--- post-commit.sh
+++ post-commit.sh
@@ -6,6 +6,7 @@ if [ $(git symbolic-ref --short HEAD) == "master" ]
 then
   echo "on master, tagging"
   tag_name=`python -c "print('v'+'.'.join(open('VERSION').readlines()[0].split()))"`
+  git tag -a $tag_name -m "automatically tagging via post-commit hook"
   echo "tag is $tag_name"
 else
   echo "not on master, not bumping"
