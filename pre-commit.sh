7ed6356797ea90a8f9e9ea4fcedd6f9c3742ed67
Ben LaRoque <benjamin.laroque@gmail.com>
2014-05-19 14:41:26 -0700

    one more full path

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 echo "doing pre-commit hook"
+HOOKS_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 if [ $(git symbolic-ref --short HEAD) == "master" ]
 then
   echo "on master, bumping"
-  python bump_version.py
+  python $HOOKS_DIR/bump_version.py
   git add VERSION
 else
   echo "not on master, not bumping"
