33da677dac240e2226b3f0ca6bd1f7a9587deafd
Noah Oblath <nsoblath@mit.edu>
2016-01-08 17:00:10 -0500

    Made authentication non-singleton. Added parable.

--- library/authentication/authentication.cc
+++ library/authentication/authentication.cc
@@ -22,69 +22,87 @@
 #include <Windows.h>
 #endif
 
-using namespace boost::filesystem;
-using std::string;
-
 namespace scarab
 {
+    using namespace boost::filesystem;
+    using std::string;
+
 
     LOGGER( mtlog, "authentication" );
 
-    authentication::authentication() :
+    authentication::authentication( const string& a_auth_filename, bool a_is_in_user_home ) :
                     param_node(),
-                    f_auth_filename( ".project8_authentications.json" ),
+                    f_auth_filename(),
                     f_is_loaded( false )
     {
-        // get the username
-        string t_username;
-        const size_t t_uname_bufsize = 1024;
-        char t_username_buf[ t_uname_bufsize ];
+        load( a_auth_filename, a_is_in_user_home );
+    }
+
+    authentication::~authentication()
+    {
+    }
+
+    bool authentication::load( const string& a_auth_file, bool a_is_in_user_home )
+    {
+        this->clear();
+
+        path t_auth_file_path( a_auth_file );
+
+        if( a_is_in_user_home )
+        {
+            // get the username
+            string t_username;
+            const size_t t_uname_bufsize = 1024;
+            char t_username_buf[ t_uname_bufsize ];
 #ifndef _WIN32
-        if( getlogin_r( t_username_buf, t_uname_bufsize ) == 0 )
+            if( getlogin_r( t_username_buf, t_uname_bufsize ) == 0 )
 #else
-        DWORD t_bufsize_win = t_uname_bufsize;
-        if( GetUserName( t_username_buf, &t_bufsize_win ) )
+            DWORD t_bufsize_win = t_uname_bufsize;
+            if( GetUserName( t_username_buf, &t_bufsize_win ) )
 #endif
-        {
-            t_username = string( t_username_buf );
-        }
-        else
-        {
-            WARN( mtlog, "Unable to get the username; authentications not loaded" );
-        }
-
-        // get the user home directory
-        path t_auth_file_path;
-        if( ! t_username.empty() )
-        {
-            string t_username = string( t_username_buf );
-
-#ifndef _WIN32
-            long t_pwd_bufsize = sysconf( _SC_GETPW_R_SIZE_MAX );
-            if ( t_pwd_bufsize == -1 )  t_pwd_bufsize = 16384; // Should be more than enough
-
-            char* t_pwd_buf = ( char* )malloc( (size_t)t_pwd_bufsize );
-            if( t_pwd_buf == NULL )
             {
-                WARN( mtlog, "Unable to allocate the pwd buffer; authentications not loaded")
+                t_username = string( t_username_buf );
             }
             else
             {
-                struct passwd t_pwd;
-                struct passwd* t_pwd_result;
-                getpwnam_r( t_username.c_str(), &t_pwd, t_pwd_buf, t_pwd_bufsize, &t_pwd_result );
-                if( t_pwd_result == NULL )
+                ERROR( mtlog, "Unable to get the username; authentications not loaded" );
+                return false;
+            }
+
+            // get the user home directory
+            if( ! t_username.empty() )
+            {
+                string t_username = string( t_username_buf );
+
+#ifndef _WIN32
+                long t_pwd_bufsize = sysconf( _SC_GETPW_R_SIZE_MAX );
+                if ( t_pwd_bufsize == -1 )  t_pwd_bufsize = 16384; // Should be more than enough
+
+                char* t_pwd_buf = ( char* )malloc( (size_t)t_pwd_bufsize );
+                if( t_pwd_buf == NULL )
                 {
-                    WARN( mtlog, "Unable to get the pwd data; authentications not loaded" );
+                    ERROR( mtlog, "Unable to allocate the pwd buffer; authentications not loaded");
+                    return false;
                 }
                 else
                 {
-                    t_auth_file_path = string( t_pwd.pw_dir ) / f_auth_filename;
+                    struct passwd t_pwd;
+                    struct passwd* t_pwd_result;
+                    getpwnam_r( t_username.c_str(), &t_pwd, t_pwd_buf, t_pwd_bufsize, &t_pwd_result );
+                    if( t_pwd_result == NULL )
+                    {
+                        ERROR( mtlog, "Unable to get the pwd data; authentications not loaded" );
+                        return false;
+                    }
+                    else
+                    {
+                        t_auth_file_path = string( t_pwd.pw_dir ) / f_auth_filename;
+                    }
                 }
-            }
 #else
-            t_auth_file_path = path( getenv( "HOMEDRIVE" ) ) / path( getenv( "HOMEPATH" ) ) / f_auth_filename;
+                t_auth_file_path = path( getenv( "HOMEDRIVE" ) ) / path( getenv( "HOMEPATH" ) ) / f_auth_filename;
 #endif
+            }
         }
 
         // is the file there?
@@ -97,12 +115,14 @@ namespace scarab
                 t_auth_file_present = exists( t_auth_file_path ) && is_regular_file( t_auth_file_path );
                 if( ! t_auth_file_present )
                 {
-                    WARN( mtlog, "File either doesn't exist (" << exists( t_auth_file_path ) << ") or isn't a regular file (" << is_regular_file( t_auth_file_path ) << ")" );
+                    ERROR( mtlog, "File either doesn't exist (" << exists( t_auth_file_path ) << ") or isn't a regular file (" << is_regular_file( t_auth_file_path ) << ")" );
+                    return false;
                 }
             }
             catch( filesystem_error& e )
             {
-                WARN( mtlog, "Unable to determine if the authentication file is a regular file: " << e.what() );
+                ERROR( mtlog, "Unable to determine if the authentication file is a regular file: " << e.what() );
+                return false;
             }
         }
 
@@ -112,7 +132,8 @@ namespace scarab
             param_node* t_read_file = param_input_json::read_file( t_auth_file_path.string() );
             if( t_read_file == NULL )
             {
-                WARN( mtlog, "Unable to parse authentication file" );
+                ERROR( mtlog, "Unable to parse authentication file" );
+                return false;
             }
             else
             {
@@ -120,16 +141,14 @@ namespace scarab
                 delete t_read_file;
                 DEBUG( mtlog, "Authentications:\n" << *this );
                 f_is_loaded = true;
+                return true;
             }
         }
         else
         {
             WARN( mtlog, "Authentications not loaded" );
+            return false;
         }
-    }
-
-    authentication::~authentication()
-    {
     }
 
 } /* namespace mantis */
--- library/authentication/authentication.hh
+++ library/authentication/authentication.hh
@@ -8,41 +8,28 @@
 #ifndef SCARAB_AUTHENTICATION_HH_
 #define SCARAB_AUTHENTICATION_HH_
 
+#include "member_variables.hh"
 #include "param.hh"
-#include "singleton.hh"
 #include "scarab_api.hh"
 
 namespace scarab
 {
 
-    class SCARAB_API authentication : public singleton< authentication >, public param_node
+    class SCARAB_API authentication : public param_node
     {
         public:
-            const std::string& get_auth_filename() const;
-            bool is_loaded() const;
-
-        private:
-            std::string f_auth_filename;
-            bool f_is_loaded;
-
-        protected:
-            friend class singleton< authentication >;
-            friend class destroyer< authentication >;
-            authentication();
+            authentication( const std::string& a_auth_file, bool a_is_in_user_home = true );
             ~authentication();
+
+        public:
+            bool load( const std::string& a_auth_file, bool a_is_in_user_home = true );
+
+        public:
+            mv_referrable( std::string, auth_filename );
+            mv_accessible_noset( bool, is_loaded );
 
     };
 
-    inline const std::string& authentication::get_auth_filename() const
-    {
-        return f_auth_filename;
-    }
-
-    inline bool authentication::is_loaded() const
-    {
-         return f_is_loaded;
-    }
-
-} /* namespace mantis */
+} /* namespace scarab */
 
 #endif /* SCARAB_AUTHENTICATION_HH_ */
