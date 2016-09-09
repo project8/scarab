d7decbbe2696436e8240ff97a585d72917e9bb61
Noah Oblath <nsoblath@gmail.com>
2016-09-09 10:35:05 -0700

    Function overloads for string exceptions in error class

--- library/utility/error.hh
+++ library/utility/error.hh
@@ -1,8 +1,9 @@
 #ifndef SCARAB_EXCEPTION_HH_
 #define SCARAB_EXCEPTION_HH_
 
-#include <sstream>
 #include <exception>
+#include <sstream>
+#include <string>
 
 #include "scarab_api.hh"
 
@@ -17,17 +18,37 @@ namespace scarab
             ~error() throw ();
 
             template< class x_streamable >
-            error& operator<<( const x_streamable& a_fragment )
-            {
-                f_error << a_fragment;
-                return *this;
-            }
+            error& operator<<( x_streamable a_fragment );
+            error& operator<<( const std::string& a_fragment );
+            error& operator<<( const char* a_fragment );
 
             virtual const char* what() const throw();
 
         private:
-            ::std::stringstream f_error;
+            std::string f_error;
     };
+
+    template< class x_streamable >
+    error& error::operator<<( x_streamable a_fragment )
+    {
+        std::stringstream stream;
+        stream << a_fragment;
+        stream >> f_error;
+        return *this;
+    }
+
+    inline error& error::operator<<( const std::string& a_fragment )
+    {
+        f_error += a_fragment;
+        return *this;
+    }
+
+    inline error& error::operator<<( const char* a_fragment )
+    {
+        f_error += std::string( a_fragment );
+        return *this;
+    }
+
 
 }
 
