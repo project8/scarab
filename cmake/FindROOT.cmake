f195eb7b71a4c9aa3a910e6b74e2d0e2f1de0683
Noah Oblath <nsoblath@gmail.com>
2016-11-25 12:46:17 -0800

    Check if ROOT_LIBRARIES has contents before removing duplicates

--- cmake/FindROOT.cmake
+++ cmake/FindROOT.cmake
@@ -49,7 +49,9 @@ foreach(_cpt ${rootlibs} ${ROOT_FIND_COMPONENTS})
     list(REMOVE_ITEM ROOT_FIND_COMPONENTS ${_cpt})
   endif()
 endforeach()
-list(REMOVE_DUPLICATES ROOT_LIBRARIES)
+if(ROOT_LIBRARIES)
+    list(REMOVE_DUPLICATES ROOT_LIBRARIES)
+endif(ROOT_LIBRARIES)
 
 execute_process(
     COMMAND ${ROOT_CONFIG_EXECUTABLE} --cflags
