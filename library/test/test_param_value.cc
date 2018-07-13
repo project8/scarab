603e6535142060071ac124092f99764d9426a307
Noah Oblath <nsoblath@gmail.com>
2018-07-13 16:01:24 -0700

    Added test_param_node.cc

--- library/test/test_param_value.cc
+++ library/test/test_param_value.cc
@@ -3,6 +3,19 @@
  *
  *  Created on: Jul 13, 2018
  *      Author: N.S. Oblath
+ *
+ *  Output should be something like this:
+ *
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(18): Bool Value Tests
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(21): Bool value via string: true
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(23): Int Value Tests
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(27): Int value via int: 5
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(28): Int value via string: 5
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(30): String Value Tests
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(32): String value: hello world
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(35): String containing a number via as_int: 10
+2018-07-13 15:25:14 [ INFO] (tid 0x7fff9c9dc380) t_param_value.cc(38): String containing a bool via as_bool: 1
+ *
  */
 
 #include "param_value.hh"
