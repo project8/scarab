861b718a920d28677c4abfb3178b822efe22aa64
Noah Oblath <nsoblath@gmail.com>
2020-03-31 18:20:10 -0700

    Added test_verbosity and updated test_logger

--- library/test/test_logger.cc
+++ library/test/test_logger.cc
@@ -13,10 +13,15 @@ LOGGER( tlog, "test_logger" );
 
 int main()
 {
+    tlog.SetGlobalLevel( logger::ELevel::eTrace );
+    std::cout << "Printing TRACE through FATAL messages" << std::endl;
+    LTRACE( tlog, "This is a TRACE message" );
     LDEBUG( tlog, "This is a DEBUG message" );
-    LINFO( tlog, "This is an LINFO( message" );
+    LINFO( tlog, "This is an LINFO message" );
+    LPROG( tlog, "This is a PROG message" );
     LWARN( tlog, "This is a WARN message" );
     LERROR( tlog, "This is an ERROR message" );
+    LFATAL( tlog, "This is a FATAL message" );
     std::cout << "This is a cout message" << std::endl;
 
     return 0;
