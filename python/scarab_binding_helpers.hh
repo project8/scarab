22264d3131860862e5e3b4ba9e509dcca07c56e2
Noah Oblath <noah.oblath@pnnl.gov>
2021-06-23 17:41:28 -0700

    Reorder a couple lines

--- python/scarab_binding_helpers.hh
+++ python/scarab_binding_helpers.hh
@@ -6,6 +6,9 @@
 #define SCARAB_BIND_CALL_GUARD_STREAMS \
     pybind11::call_guard< pybind11::scoped_ostream_redirect, pybind11::scoped_estream_redirect >()
 
+#define SCARAB_BIND_CALL_GUARD_GIL \
+    pybind11::call_guard< pybind11::gil_scoped_release >()
+
 #define SCARAB_BIND_CALL_GUARD_STREAMS_AND_GIL \
     pybind11::call_guard< pybind11::scoped_ostream_redirect, pybind11::scoped_estream_redirect, pybind11::gil_scoped_release >()
 
