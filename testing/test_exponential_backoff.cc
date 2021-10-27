a2cfdbcf2b98969390ed1c03c8db77c73333d676
Pranava Teja Surukuchi <pranavateja.surukuchi@yale.edu>
2021-10-27 17:26:04 -0400

    forgot fixing test_exponential_backoff.cc

--- testing/test_exponential_backoff.cc
+++ testing/test_exponential_backoff.cc
@@ -13,7 +13,6 @@
 
 using namespace std::chrono;
 
-// Should this be exponential-backoff and not exponential_backoff following the rest of the examples ?
 TEST_CASE( "exponential_backoff", "[utility]" )
 {
     unsigned t_value = 0; // value to increment, allowing verification that the function executed; check that this has changed from the before-test value
