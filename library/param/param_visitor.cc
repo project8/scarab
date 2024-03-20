c9b5e2c75cc8e12aa9e65c71f82dfcc42b7fb9c3
Noah Oblath <nsoblath@gmail.com>
2024-03-19 17:40:39 -0700

    Filled out param_visitor and started test

---
+++ library/param/param_visitor.cc
@@ -0,0 +1,65 @@
+/*
+ * param_visitor.cc
+ *
+ *  Created on: Mar 19, 2024
+ *      Author: N.S. Oblath
+ */
+
+#define SCARAB_API_EXPORTS
+
+#include "param_visitor.hh"
+
+#include "logger.hh"
+
+LOGGER( plog, "param_visitor" );
+
+namespace scarab
+{
+        param_visitor::param_visitor() {}
+        param_visitor::~param_visitor() {}
+
+
+        param_visitor_callback::param_visitor_callback() :
+                param_visitor(),
+                f_param_callback( [](const param&) {
+                    LDEBUG( plog, "param callback" );
+                }),
+                f_param_array_callback( [](const param_array& an_array) {
+                    LDEBUG( plog, "Param callback: " << an_array );
+                }),
+                f_param_node_callback( [](const param_node& a_node) {
+                    LDEBUG( plog, "param_node callback: " << a_node );
+                }),
+                f_param_value_callback( [](const param_value& a_value) {
+                    LDEBUG( plog, "param_value callback: " << a_value );
+                })
+        {}
+
+        param_visitor_callback::~param_visitor_callback() 
+        {}
+
+        void param_visitor_callback::operator()( const param& a_null )
+        {
+            f_param_callback( a_null );
+            return;
+        };
+
+        void param_visitor_callback::operator()( const param_array& an_array )
+        {
+            f_param_array_callback( an_array );
+            return;
+        };
+
+        void param_visitor_callback::operator()( const param_node& a_node )
+        {
+            f_param_node_callback( a_node );
+            return;
+        };
+
+        void param_visitor_callback::operator()( const param_value& a_avalue )
+        {
+            f_param_value_callback( a_value );
+            return;
+        };
+
+};
---
+++ library/param/param_visitor.hh
@@ -0,0 +1,61 @@
+/*
+ * param_visitor.hh
+ *
+ *  Created on: Mar 18, 2024
+ *      Author: N.S. Oblath
+ */
+
+#ifndef SCARAB_PARAM_VISITOR_HH_
+#define SCARAB_PARAM_VISITOR_HH_
+
+#include <functional>
+
+#include "member_variables.hh"
+#include "scarab_api.hh"
+
+namespace scarab
+{
+    class param;
+    class param_array;
+    class param_node;
+    class param_value;
+
+    class SCARAB_API param_visitor
+    {
+        public:
+            param_visitor();
+            virtual ~param_visitor();
+
+            virtual void operator()( const param& ) = 0;
+            virtual void operator()( const param_array& ) = 0;
+            virtual void operator()( const param_node& ) = 0;
+            virtual void operator()( const param_value& ) = 0;
+    };
+
+
+    class SCARAB_API param_visitor_callback : public param_visitor
+    {
+        public:
+            typedef std::function< void (const param&) > param_callback_t;
+            typedef std::function< void (const param_array&) > param_array_callback_t;
+            typedef std::function< void (const param_node&) > param_node_callback_t;
+            typedef std::function< void (const param_value&) > param_value_callback_t;
+
+            param_visitor_callback();
+            virtual ~param_visitor_callback();
+
+            virtual void operator()( const param& );
+            virtual void operator()( const param_array& );
+            virtual void operator()( const param_node& );
+            virtual void operator()( const param_value& );
+
+        protected:
+            memvar_ref( param_callback_t, param_callback );
+            memvar_ref( param_array_callback_t, param_array_callback );
+            memvar_ref( param_node_callback_t, param_node_callback );
+            memvar_ref( param_value_callback_t, param_value_callback );
+    };
+
+} /* namespace scarab */
+
+#endif /* SCARAB_PARAM_VISITOR_HH_ */
