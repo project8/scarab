eac6eff0a361074d7c41466b68d2ec4f9a5124bd
Noah Oblath <nsoblath@gmail.com>
2024-05-21 16:32:58 -0700

    Updated main_app pybind

--- python/application_pybind.hh
+++ python/application_pybind.hh
@@ -35,10 +35,12 @@ namespace scarab_pybind
             .def_property_readonly( "use_config", &scarab::main_app::get_use_config )
             .def_property( "auth_spec_key", (std::string& (scarab::main_app::*)()) &scarab::main_app::auth_spec_key,
                            [](scarab::main_app& an_obj, std::string& a_key){ an_obj.auth_spec_key() = a_key; } )
-            .def_property( "auth_file_key", (std::string& (scarab::main_app::*)()) &scarab::main_app::auth_file_key,
-                           [](scarab::main_app& an_obj, std::string& a_key){ an_obj.auth_file_key() = a_key; } )
             .def_property( "auth", (scarab::authentication& (scarab::main_app::*)()) &scarab::main_app::auth,
                            [](scarab::main_app& an_obj, scarab::authentication& an_auth){ an_obj.auth() = an_auth; } )
+
+            .def( "set_default_auth_spec", &scarab::main_app::set_default_auth_spec )
+            .def( "set_default_auth_spec_group", &scarab::main_app::set_default_auth_spec_group )
+            .def( "set_default_auth_file", &scarab::main_app::set_default_auth_file )
 
             .def( "do_config_stage_1", &scarab::main_app::do_config_stage_1, SCARAB_BIND_CALL_GUARD_STREAMS )
             .def( "do_config_stage_2", &scarab::main_app::do_config_stage_2, SCARAB_BIND_CALL_GUARD_STREAMS )
