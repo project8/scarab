efb91056506cc010134f29f3fdae2fe47274a6aa
Noah Oblath <nsoblath@gmail.com>
2024-05-20 17:28:20 -0700

    Add pybinding for new has/get functions

--- python/authentication_pybind.hh
+++ python/authentication_pybind.hh
@@ -12,6 +12,11 @@ namespace scarab_pybind
     {
         std::list< std::string > all_items;
 
+        using has_sig1 = bool(scarab::authentication::*)( const std::string& ) const;
+        using has_sig2 = bool(scarab::authentication::*)( const std::string&, const std::string& ) const;
+        using get_sig1 = std::string(scarab::authentication::*)( const std::string&, const std::string& ) const;
+        using get_sig2 = std::string(scarab::authentication::*)( const std::string&, const std::string&, const std::string& ) const;
+
         all_items.push_back( "Authentication" );
         pybind11::class_< scarab::authentication >( mod, "Authentication", "class for defining and processing authentication information" )
             .def( pybind11::init<>() )
@@ -24,8 +29,10 @@ namespace scarab_pybind
             .def( "process_spec", &scarab::authentication::process_spec, SCARAB_BIND_CALL_GUARD_STREAMS )
             .def_property( "spec", (scarab::param_node& (scarab::authentication::*)()) &scarab::authentication::spec,
                            [](scarab::authentication& an_obj, const scarab::param_node& a_spec ) {an_obj.spec() = a_spec; } )
-
-            .def( "get", &scarab::authentication::get, pybind11::arg("group"), pybind11::arg("item") )
+            .def( "has", static_cast<has_sig1>(&scarab::authentication::has) )
+            .def( "has", static_cast<has_sig2>(&scarab::authentication::has) )
+            .def( "get", static_cast<get_sig1>(&scarab::authentication::get) )
+            .def( "get", static_cast<get_sig2>(&scarab::authentication::get) )
             .def_property( "data", (scarab::param_node& (scarab::authentication::*)()) &scarab::authentication::data,
                            [](scarab::authentication& an_obj, const scarab::param_node& a_data ) {an_obj.data() = a_data; } )
 
