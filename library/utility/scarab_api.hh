43b6c5e8e668a20c72036e0b74b0237e6697db10
Noah Oblath <nsoblath@gmail.com>
2024-08-28 09:51:19 -0700

    Modifying SCARAB_API to also deal with symbol visibility for Pybind11

--- library/utility/scarab_api.hh
+++ library/utility/scarab_api.hh
@@ -11,7 +11,20 @@
 
 namespace scarab
 {
-    // API export macros for windows
+    /**
+     * SCARAB_API is used to modify the visibility of classes and functions for:
+     *   - Windows DLLs
+     *   - Pybind11
+     * 
+     * For Windows DLLs, the API needs to be exported to make portable DLLs.
+     *  __delclspec() changes the behavior of the `SCARAB_API` macro depending on whether
+     * it's being used in a header file (dllimport) or source file (dllexport).
+     * 
+     * For Pybind11, when using gcc or clang, if you want to be able to use the classes in another package 
+     * (i.e. some other package will use the functions/classes you're binding), then you need to 
+     * make them visible.  See https://pybind11.readthedocs.io/en/stable/advanced/misc.html#partitioning-code-over-multiple-extension-modules
+     * for more information.  This macro is taking the place of `PYBIND11_EXPORT`.
+    */
 #ifdef _WIN32
 #  ifdef SCARAB_API_EXPORTS
 #    define SCARAB_API __declspec(dllexport)
@@ -21,7 +34,11 @@ namespace scarab
 #    define SCARAB_EXPIMP_TEMPLATE extern
 #  endif
 #else
-#  define SCARAB_API
+#  ifdef SCARAB_API_EXPORTS
+#    define SCARAB_API
+#  else
+#    define SCARAB_API __attribute__((visibility("default")))
+#  endif
 #endif
 
 }
